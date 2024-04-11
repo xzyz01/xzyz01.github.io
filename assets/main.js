fed64233f249b70a919ce99d0d2049ad6314aafe
YzYz01 <164509032+YzYz01@users.noreply.github.com>
2024-04-11 11:27:00 +0200

    Update

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,3 @@
-
 AOS.init({
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
@@ -9,3 +8,28 @@ AOS.init({
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
+
+function togglePopup(message) {
+  document.getElementById("popup-1").classList.toggle("active");
+  let title = document.getElementById("titlePopup")
+  let content = document.getElementById("contentPopup")
+  switch (message) {
+    case 1:
+      title.innerHTML = 'Diseño página sign in - 2024 (Google)';
+      content.innerHTML = 'Colaboramos estrechamente con Google para llevar a cabo una renovación total del diseño de la página de registro y acceso. Nuestro enfoque se centró en mejorar la usabilidad, la accesibilidad y la estética, dando a los usuarios una experiencia más intuitiva y satisfactoria al interactuar con la plataforma.';
+      console.log("case1");
+      break;
+    case 2:
+      title.innerHTML = 'Montaje CPD Azure - 2022 (Microsoft)';
+      content.innerHTML = 'Nos encargamos de la planificación, instalación y configuración completa de los equipos informáticos necesarios para un nuevo centro de procesamiento de datos en la nube de Microsoft Azure. Nuestro equipo técnico trabajó de manera eficiente y meticulosa para garantizar un buen rendimiento y seguridad en esta infraestructura.';
+      console.log("case2");
+      break;
+    case 3:
+      title.innerHTML = 'Face ID - 2017';
+      content.innerHTML = 'Como parte de un equipo colaborativo, contribuimos al diseño, desarrollo y posterior implementación del innovador sistema de reconocimiento facial, Face ID, para dispositivos de Apple. Nuestra participación abarcó desde la fase conceptual hasta la entrega final, demostrando nuestra capacidad para innovar y adaptarnos a las nuevas tecnologías.';
+      console.log("case3");
+      break;
+    default:
+      break;
+  }
+}
